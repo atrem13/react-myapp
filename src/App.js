6cc7fc3ad67b04cf205eabaac8a79a4520a99870
atrem13 <mertayoga17.my@gmail.com>
2021-10-05 18:06:52 +0800

    crud done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
-    <div>
-      <Header/>  
-    </div> 
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <Router>
+                <Switch>
+                  <Route exact path="/">
+                    <ProductList />
+                  </Route>
+                  <Route path="/add">
+                    <AddProduct />
+                  </Route>
+                  <Route path="/edit/:id">
+                    <EditProduct />
+                  </Route>
+                </Switch>
+              </Router>
+            </div>
+          </div>
+        </div>
   );
 }
 
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    )
-}
-
-export default Header
